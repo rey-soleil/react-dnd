a8d798bd325f42f359832660e33dca6f18ec567c
rey-soleil <68834278+rey-soleil@users.noreply.github.com>
2023-07-25 11:24:19 -0700

    style: move scroll logic into useEffect

--- src/components/Zooming/Container.tsx
+++ src/components/Zooming/Container.tsx
@@ -9,6 +9,7 @@ type Props = {
 }
 export default function ZoomableContainer({ children }: Props) {
   const [zoom, setZoom] = useAtom(zoomAtom)
+
   usePinch(
     ({ offset: [d] }) => {
       setZoom(d)
@@ -28,32 +29,36 @@ export default function ZoomableContainer({ children }: Props) {
     document.addEventListener('gesturestart', handler)
     document.addEventListener('gesturechange', handler)
     document.addEventListener('gestureend', handler)
+
+    const board = document.querySelector('.board')
+    const stickies = document.querySelectorAll(
+      '.top-sticky',
+    ) as NodeListOf<HTMLElement>
+
+    // Manually set the top of the sticky notes after a zoom or scroll.
+    const setTop = () => {
+      if (!board) return
+      let offset = board.scrollTop - board.scrollTop / zoom
+      stickies.forEach((sticky) => (sticky.style.top = `${-offset}px`))
+    }
+
+    board?.addEventListener('scroll', setTop)
+
+    // Cleanup
     return () => {
       document.removeEventListener('gesturestart', handler)
       document.removeEventListener('gesturechange', handler)
       document.removeEventListener('gestureend', handler)
+      board?.removeEventListener('scroll', setTop)
     }
-  }, [])
-
-  let board = document.querySelector('.board')
-  let stickies = document.querySelectorAll(
-    '.top-sticky',
-  ) as NodeListOf<HTMLElement>
-
-  board?.addEventListener('scroll', setTop)
-
-  function setTop() {
-    if (!board) return
-    let offset = board.scrollTop - board.scrollTop / zoom
-    stickies.forEach((sticky) => (sticky.style.top = `${-offset}px`))
-  }
+  }, [zoom]) // adding zoom as dependency since setTop uses it
 
   return (
     <div
       style={{
         transform: `scale(${zoom})`,
         transformOrigin: 'top left',
-        // zoom: isVisible ? 1 : zoom,
+        // zoom: isVisible ? 1 : zoom, // commented out, unsure if you want this?
       }}
     >
       {children}

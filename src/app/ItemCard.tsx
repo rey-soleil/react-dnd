c9cb8bb5ed18fd15d51b4468b253d91a2b72068c
ShrJamal <me@shrjamal.com>
2023-07-26 21:22:59 +0100

    chore: hide add button on card item

--- src/app/ItemCard.tsx
+++ src/app/ItemCard.tsx
@@ -16,12 +16,12 @@ type Props = {
 export default function ItemCard({ item }: Props) {
   const divRef = useRef<HTMLDivElement>(null)
   const { mutate: addItem } = useAddItemMutation()
-
+  const isHeader = item.row === 0
   return (
     <div
       className={clsx(
         'w-[16rem] p-2 flex flex-col relative text-center ',
-        item.row === 0 ? 'h-[5rem]' : 'h-[10rem]',
+        isHeader ? 'h-[5rem]' : 'h-[10rem]',
       )}
       style={{
         boxShadow: 'rgba(0, 0, 0, 0.35) 0px 5px 15px',
@@ -37,18 +37,20 @@ export default function ItemCard({ item }: Props) {
       >
         <p>{item.content}</p>
       </div>
-      <button
-        className="btn btn-xs btn-circle btn-ghost absolute bottom-1 right-1"
-        onClick={() =>
-          addItem({
-            type: 'items',
-            column: item.column,
-            content: `Item ${item.column} - ${item.row + 1}`,
-          })
-        }
-      >
-        <Icons.Add />
-      </button>
+      {isHeader && (
+        <button
+          className="btn btn-xs btn-circle btn-ghost absolute bottom-1 right-1"
+          onClick={() =>
+            addItem({
+              type: 'items',
+              column: item.column,
+              content: `Item ${item.column} - ${item.row + 1}`,
+            })
+          }
+        >
+          <Icons.Add />
+        </button>
+      )}
     </div>
   )
 }

410157723abb8d42a58006a8a09a9844c153de24
rey-soleil <68834278+rey-soleil@users.noreply.github.com>
2023-07-27 15:38:10 -0700

    test: restore offset functionality

--- src/components/Zooming/Container.tsx
+++ src/components/Zooming/Container.tsx
@@ -14,23 +14,8 @@ export default function ZoomableContainer({ children }: Props) {
   // `movement` stores the offset since you started pinching.
   // Eg. if you zoom to 5, stop, and start again, movement will reset to 1.
   usePinch(
-    (props) => {
-      const movement = props.movement[0]
-
-      // Did the user just reset the zoom?
-      // If so, set mostRecentZoom to 1.
-      // If not, multiply the most recent zoom by the movement.
-      if (zoom === 1) {
-        setMostRecentZoom(1)
-        setZoom(movement)
-      } else {
-        setZoom(mostRecentZoom * movement)
-      }
-
-      // User has stopped pinching, so set most recent zoom.
-      if (props.delta[0] === 0) {
-        setMostRecentZoom(mostRecentZoom * movement)
-      }
+    ({ offset: [d] }) => {
+      setZoom(d)
     },
     {
       target: typeof document !== 'undefined' ? document : undefined,
